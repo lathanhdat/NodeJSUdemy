8a1002cf2e037c5ca27750b5f8e0815f2664b1df
Dat <dat.la@terralogic.com>
2019-03-25 14:52:21 +0700

    update asyns/await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -18,71 +18,70 @@ appExpress.post('/users',async(req,res)=>{
     catch(e){
         res.status(400).send(e)
     }
-    
-    // user.save().then(()=>{
-    //     res.status(201).send(user)
-    // }).catch((err)=>{
-    //     res.status(400).send(err)
-    // })
 })
 
-appExpress.get('/users',(req,res)=>{
+appExpress.patch('/users/:id',async (req,res)=>{
+    const _id = req.params.id;
+    try {
+        console.log(_id)
+        console.log(req.body)
+        const user = await User.findByIdAndUpdate(_id,req.body,{new : true, runValidators:true})
+        if(!user) return res.status(404).send()
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+appExpress.get('/users',async (req,res)=>{
     try {
         const users = await User.find({})
         res.send(users)
     } catch (error) {
         res.status(500).send(error)
     }
-    // User.find({}).then((users)=>{
-    //     res.send(users)
-    // }).catch((err)=>{
-    //     res.status(500).send(err)
-    // })
 })
 
-appExpress.get('/users/:id',(req,res)=>{
+appExpress.get('/users/:id',async (req,res)=>{
     const _id = req.params.id;
     try {
-        const user = User.findById(_id)
+        const user = await User.findById(_id)
         if (!user) return res.status(404).send()
         res.send(user)
     } catch (error) {
         res.status(500).send()
     }
-    // User.findById(_id).then((user)=>{
-    //     if(!user) return res.status(404).send()
-    //     res.send(user)
-    // }).catch((err)=>{
-    //     res.status(500).send(err)
-    // })
 })
 
 //Task end point
-appExpress.post('/tasks',(req,res)=>{
+appExpress.post('/tasks',async (req,res)=>{
     const task = new Task(req.body)
-    task.save().then(()=>{
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((err)=>{
+    } catch (error) {
         res.status(400).send(err)
-    })
+    }
 })
 
-appExpress.get('/tasks',(req,res)=>{
-    Task.find({}).then((tasks)=>{
+appExpress.get('/tasks',async (req,res)=>{
+    try {
+        const tasks = await Task.find({})
         res.send(tasks)
-    }).catch((err)=>{
+    } catch (error) {
         res.status(500).send(err)
-    })
+    }
 })
 
-appExpress.get('/tasks/:id',(req,res)=>{
+appExpress.get('/tasks/:id',async (req,res)=>{
     const _id = req.params.id;
-    Task.findById(_id).then((task)=>{
+    try {
+        const task = await Task.findById(_id)
         if(!task) return res.status(404).send()
         res.send(task)
-    }).catch((err)=>{
+    } catch (error) {
         res.status(500).send(err)
-    })
+    }
 })
 
 appExpress.listen(port,()=>{
